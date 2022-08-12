1d6450d26ff7129b97d26e249d5c8849abd09224
kgeeti <kge@terra.com.br>
2022-08-12 14:41:40 -0300

    FastFood API v1.01 PedidoService: Removido imports não utilizados PedidoService: Alterado retorno da salvar de void para Pedido.
    Incluso diagrama UML via PLantUML

--- src/main/java/br/eti/kge/fastfood/domain/service/PedidoService.java
+++ src/main/java/br/eti/kge/fastfood/domain/service/PedidoService.java
@@ -8,10 +8,7 @@ package br.eti.kge.fastfood.domain.service;
 
 import br.eti.kge.fastfood.domain.model.Pedido;
 import br.eti.kge.fastfood.domain.model.StatusPedido;
-import br.eti.kge.fastfood.domain.repository.ClienteRepository;
 import br.eti.kge.fastfood.domain.repository.PedidoRepository;
-import br.eti.kge.fastfood.domain.repository.ProdutoRepository;
-import ch.qos.logback.core.CoreConstants;
 import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -29,12 +26,12 @@ public class PedidoService {
     @Autowired
     private PedidoRepository pedidoRepository;
     
-    public void salvar(Pedido pedido) {
+    public Pedido salvar(Pedido pedido) {
         // Atualiza campos abertoEm e status
         pedido.setAbertoEm(LocalDateTime.now());
         pedido.setStatus(StatusPedido.ABERTO);
         
-        pedidoRepository.save(pedido);
+        return pedidoRepository.save(pedido);
     }
     
 }
